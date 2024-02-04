d27af6db6e4467b7d6b48249f23e8b0a2e82e68b
AnastasiiaDubchak <frilansa1@gmail.com>
2024-02-05 00:54:21 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,39 @@ function currentTemperature(response) {
   let current = `${temperature}`;
   let showTemperature = document.querySelector(".current-temperature");
   let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#wind-speed");
+
+
   cityElement.innerHTML = response.data.city;
   showTemperature.innerHTML = current;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeed.innerHTML = `${response.data.wind.speed}km/h`;
 }
+
+//function refreshWeather(response) {
+  //let temperatureElement = document.querySelector("#temperature");
+ // let temperature = response.data.temperature.current;
+  //let cityElement = document.querySelector("#city");
+ // let descriptionElement = document.querySelector("#description");
+  //let humidityElement = document.querySelector("#humidity");
+  //let windSpeedElement = document.querySelector("#wind-speed");
+  //let timeElement = document.querySelector("#time");
+  //let date = new Date(response.data.time * 1000);
+  //let iconElement = document.querySelector("#icon");
+
+  //cityElement.innerHTML = response.data.city;
+  //timeElement.innerHTML = formatDate(date);
+  //descriptionElement.innerHTML = response.data.condition.description;
+  //humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  //windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  //temperatureElement.innerHTML = Math.round(temperature);
+  //iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
+  //getForecast(response.data.city);
+//}
 
 function search(event) {
   event.preventDefault();
